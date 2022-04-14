91948e1ad072563a4521b27d9d3194885179323c
Sanjae1 <wurlboss.empire@gmail.com>
2022-04-14 02:50:07 -0500

    yup

---
+++ components/HomeHeader.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeHeade = () => {
+  return (
+    <View>
+      <Text>HomeHeade</Text>
+    </View>
+  )
+}
+
+export default HomeHeade
